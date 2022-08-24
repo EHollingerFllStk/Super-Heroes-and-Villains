0a5659a6e768fe73f98d5e9ffe9b06d28a6110c7
Beth-Hollinger <hollingere15@gmail.com>
2022-08-24 19:55:11 -0400

    remove console.logs

--- js/script.js
+++ js/script.js
@@ -37,22 +37,12 @@ function handleGetData(event) {
 
     $.ajax(`https://superheroapi.com/api.php/10159819421940743/search/${userInput}`).then(
         function (data) {
-            console.log("the eagle has landed");
-            console.log('stats', data.results)
-            //console.log(data)
             $name.text(data.results[0].name)
-            console.log(data.results[0].name)
-            console.log(data.results[0].powerstats.combat)
             $combat.text(data.results[0].powerstats.combat)
-            console.log(data.results[0].powerstats.durability)
             $durability.text(data.results[0].powerstats.durability)
-            console.log(data.results[0].powerstats.intelligence)
             $intelligence.text(data.results[0].powerstats.intelligence)
-            console.log(data.results[0].powerstats.power)
             $power.text(data.results[0].powerstats.power)
-            console.log(data.results[0].powerstats.speed)
             $speed.text(data.results[0].powerstats.speed)
-            console.log(data.results[0].powerstats.strength)
             $strength.text(data.results[0].powerstats.strength)
             $image.attr("src", data.results[0].image["url"])
             $input.val('')
